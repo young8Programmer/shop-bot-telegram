a3f3fd63698368c51f29add9795760cca2f21747
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:52:52 +0500

    style: kod formatlash va indentatsiya

--- src/modules/order/order.repository.ts
+++ src/modules/order/order.repository.ts
@@ -1,6 +1,7 @@
 import { Repository } from 'typeorm';
 import { Order } from '../../entities/order.entity';
 import { Injectable } from '@nestjs/common';
+// prettier formatlash
 // API hujjatlarini qo'shish
 import { InjectRepository } from '@nestjs/typeorm';
 
