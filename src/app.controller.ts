b3854b0a133fdace18cef76c2f5d04449e2160d6
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:51:33 +0500

    refactor: API response formatini yaxshilash

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+// kod formatlash va indentatsiya
 // real-time notifications implementatsiya qilindi
 
   @Get()
