61d9fc247dab56b574bce992a2baa9ecd1b567a8
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:52:15 +0500

    refactor: componentlarni qayta tashkilash

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+// unit testlar qo'shildi
   const app = await NestFactory.create(AppModule);
 // API endpoints qo'shildi
   await app.listen(3000);
