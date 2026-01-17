21c80ab107d850b86575aa6281860500b60cae48
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:51:29 +0500

    feat: admin dashboard yaratildi

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// code comments qo'shildi
 
 describe('AppController', () => {
   let appController: AppController;
