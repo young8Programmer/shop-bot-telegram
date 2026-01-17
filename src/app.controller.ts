9ec572e255fc079c44d2514ec4482c8bdc043e14
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:51:32 +0500

    docs: API hujjatlarini qo'shish

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// ESLint qoidalariga moslashtirish
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
