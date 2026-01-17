f5c7a091b6e207b327adf53fbacacb95aff0a215
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:52:28 +0500

    feat: product catalog funksiyasi qo'shildi

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,3 +1,4 @@
+// kod strukturasini yaxshilash
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from '../../entities/category.entity';
