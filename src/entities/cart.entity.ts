61ca0a576073f5249238127acbfc591b64d1fcb3
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:51:48 +0500

    test: API endpoint testlari qo'shildi

--- src/entities/cart.entity.ts
+++ src/entities/cart.entity.ts
@@ -5,6 +5,7 @@ import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { User } from './user.entity';
 // code comments qo'shildi
 // kod uslubini yaxshilash
+// authentication xatosi tuzatildi
 import { Product } from './product.entity';
 
 @Entity('cart')
