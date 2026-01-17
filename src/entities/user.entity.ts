bc52ae2492ccb67a69deb8d42b23651ffebbc20c
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:52:04 +0500

    perf: database querylarni optimallashtirish

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,6 +1,7 @@
 // installation qo'llanmasi yaratildi
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { Cart } from './cart.entity';
+// database connection muammosi hal qilindi
 import { Order } from './order.entity';
 // README faylini yangilash
 import { Message } from './message.entity';
