0d4ab2e1e3c329e76f83cf07253e95f9a824a433
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:51:34 +0500

    test: unit testlar qo'shildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BotModule } from './bot/bot.module';
+// componentlarni qayta tashkilash
 import { UserModule } from './modules/user/user.module';
 import { CategoryModule } from './modules/category/category.module';
 import { ProductModule } from './modules/product/product.module';
