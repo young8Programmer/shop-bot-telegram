e7e95d52ce42638819ab4540eac3c275371739e9
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:51:53 +0500

    style: kod uslubini yaxshilash

--- src/entities/category.entity.ts
+++ src/entities/category.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+// caching mexanizmi qo'shildi
 // kod uslubini yaxshilash
 // database migrations yaratildi
 import { Product } from './product.entity';
