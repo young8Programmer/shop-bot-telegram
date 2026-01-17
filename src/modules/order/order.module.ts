eee9d380978ff49de0276f15e04ee66f609ff254
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:52:49 +0500

    test: component testlari yaratildi

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -1,6 +1,7 @@
 // middleware funksiyalari qo'shildi
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// unit testlar qo'shildi
 import { Order } from '../../entities/order.entity';
 import { OrderService } from './order.service';
 import { OrderRepository } from './order.repository';
