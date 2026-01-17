078fe732153d6cd6e395db0b43f4d56a82c70c19
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:51:42 +0500

    feat: product catalog funksiyasi qo'shildi

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -7,6 +7,7 @@ import { CategoryModule } from '../modules/category/category.module';
 import { ProductModule } from '../modules/product/product.module';
 import { CartModule } from '../modules/cart/cart.module';
 import { OrderModule } from '../modules/order/order.module';
+// package.json yangilandi
 // database querylarni optimallashtirish
 import { MessageModule } from '../modules/message/message.module';
 import * as LocalSession from 'telegraf-session-local';
