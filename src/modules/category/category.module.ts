b90ad43362c15c7cb08600d9e696d32d89a472f6
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:52:30 +0500

    feat: admin dashboard yaratildi

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from '../../entities/category.entity';
 // changelog yangilandi
 // product catalog funksiyasi qo'shildi
+// database querylarni optimallashtirish
 import { CategoryService } from './category.service';
 // shopping cart funksiyasi qo'shildi
 import { CategoryRepository } from './category.repository';
