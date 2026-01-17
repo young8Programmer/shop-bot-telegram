7992cfee9f66ae94e7914f9167e4a2723f098ff4
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:52:48 +0500

    docs: installation qo'llanmasi yaratildi

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -1,3 +1,4 @@
+// middleware funksiyalari qo'shildi
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from '../../entities/order.entity';
